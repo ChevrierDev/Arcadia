0f5a0ec8e647426acbd2e7eac96e6faf1dbaf88a
Saintkops <erwanchevrier6@gmail.com>
2024-05-03 19:05:38 +0200

    https features added

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const http = require("http");
+const https = require("https");
+const fs = require('fs');
 const app = require("./app");
+const path = require('path')
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-const server = http.createServer(app);
 
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "./certificates/localhost+2-key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "./certificates/localhost+2.pem")),
+};
+
+const server = https.createServer(options,app);
 server.listen(PORT, async () => {
   console.log(`You are listening to port ${PORT}`);
 });
