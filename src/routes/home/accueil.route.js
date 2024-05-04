34ee38a3384d1b38c6ed0b14bc59e06de9fb6755
Saintkops <erwanchevrier6@gmail.com>
2024-05-04 21:32:30 +0200

    home page header added

--- src/routes/home/accueil.route.js
+++ src/routes/home/accueil.route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const accueilRouter = express.Router();
 
 accueilRouter.get('/', (req, res) => {
-    res.render('layouts/accueil')
+    res.render('layouts/accueil', {
+        title: "Page d'accueil"
+    })
 });
 
 module.exports = accueilRouter;
